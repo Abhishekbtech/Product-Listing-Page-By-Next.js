d294507ed27277b0246fdb98ae305e852ed704e0
Abhishek Chauhan <abhishekb.tech271199@gmail.com>
2024-05-31 21:19:42 +0530

    update

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -14,15 +14,17 @@ interface ProductCardProps {
 const ProductCard: React.FC<ProductCardProps> = ({ product, onClick }) => {
     return (
         <div
-            className="border p-4 bg-white text-black rounded cursor-pointer hover:shadow-lg transition duration-400 flex flex-col justify-between"
-            onClick={onClick}
-        >
-            <img src={product.image} alt={product.title} className="w-full h-60 object-cover mb-2" />
-            <div>
-                <h2 className="text-lg font-bold">{product.title}</h2>
-                <p className="text-gray-700 mb-5 mt-5">${product.price}</p>
-            </div>
-        </div>
+    className="border p-4 bg-white text-black rounded cursor-pointer flex flex-col justify-between transform transition-transform duration-300 hover:shadow-lg hover:scale-105"
+    onClick={onClick}
+>
+    <img src={product.image} alt={product.title} className="w-full h-60 object-cover mb-2" />
+    <div>
+        <h2 className="text-lg font-bold">{product.title}</h2>
+        <p className="text-gray-700 mb-5 mt-5">${product.price}</p>
+    </div>
+</div>
+
+
     )
 }
 
